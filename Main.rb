b17bf9f08954602aa24b1e52701f7654c1680ca7
Pawel Micun <pawel.micun@gmail.com>
2014-04-08 18:53:56 -0400

    data reading and basic stats

--- Main.rb
+++ Main.rb
@@ -1,29 +1,102 @@
-require 'set'
-require 'sqlite3'
-require 'active_record'
+require 'pp'
 
-require_relative 'Movie'
-require_relative 'Rating'
-
-ActiveRecord::Base.establish_connection(
-  adapter:  'sqlite3',
-  database: 'test.db',
-)
-
-# ActiveRecord::Base.logger = Logger.new(STDOUT)
-
-Rating.initDB('./nf_prize_dataset/training_set')
-
-puts Rating.count
-
-Rating.where(customer: 862759).each do |r|
-	puts r
+def Entropy(records)
+	sum = 0.0
+	sz = records.size.to_f
+	
+	records.group_by{|r| r.klass}.each do |k, v|
+		sum += (v.size / sz) * Math.log(v.size / sz, 2)
+	end
+	
+	-sum
 end
 
-movies = Hash.new
-
-Movie.list('./nf_prize_dataset/movie_titles.txt') do |m|
-	movies[m.id] = m
+def GINI(records)
+	sum = 0.0
+	sz = records.size.to_f
+	
+	records.group_by{|r| r.klass}.each do |k, v|
+		sum += (v.size / sz) ** 2
+	end
+	
+	1.0 - sum
 end
 
-puts movies.size
+class Record
+	attr_accessor :id
+	attr_accessor :attrs
+	attr_accessor :klass
+end
+
+out_map = {'2' => 1, '4' => -1}
+
+data_file = 'breast-cancer-wisconsin.data'
+train_perc = 0.5
+
+train = []
+test = []
+
+File.open(data_file) do |f|
+	records = Hash.new
+	seq = (1..100).each
+	rng = Random.new(3211)
+
+	while line = f.gets
+		if line.include? ',?,'
+			puts 'skipping: ' + line
+		else
+			split = line.strip.split(',')
+			
+			r = Record.new()
+			r.id = split[0]
+			r.attrs = split[1,split.size - 2].map{|x| x.to_i}
+			r.klass = out_map[split[-1]]
+			
+			if records.key?(r.id)
+				puts 'duplicate record id: ' + r.id.to_s
+				r.id = r.id + '_' + seq.next.to_s
+			end
+			
+			records[r.id] = r
+			
+			# pp r
+			
+			if rng.rand < train_perc
+				train << r
+			else
+				test << r
+			end
+		end
+	end
+end
+
+puts train.size
+puts test.size
+
+
+c1 = Record.new
+c1.klass = 'c1'
+
+c2 = Record.new
+c2.klass = 'c2'
+
+puts GINI([] << c2 << c2 << c2 << c2)
+puts GINI([] << c1 << c2 << c2 << c2 << c2 << c2)
+puts GINI([] << c1 << c1 << c2 << c2 << c2 << c2)
+puts GINI([] << c1 << c1 << c1 << c2 << c2 << c2)
+
+puts Entropy([] << c2 << c2 << c2 << c2)
+puts Entropy([] << c1 << c2 << c2 << c2 << c2 << c2)
+puts Entropy([] << c1 << c1 << c2 << c2 << c2 << c2)
+
+puts '============================'
+
+puts GINI(train)
+puts GINI(test)
+
+puts Entropy(train)
+puts Entropy(test)
+
+
+# puts records.size	683
+
---
+++ jruby_console.bat
@@ -0,0 +1,5 @@
+set JRUBY_OPTS="--1.9 --server -J-Xmx6G -J-Xmn1G -J-XX:ReservedCodeCacheSize=128M -J-XX:+UseConcMarkSweepGC --manage"
+@set PATH=C:\jruby-1.7.11\bin;%PATH%
+@cmd
+
+@rem trinidad -t
