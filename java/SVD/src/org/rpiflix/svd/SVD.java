b06d0cd3d8e4c5c282ff27b9871dd117b8481c36
PawelMicun <pawel.micun@gmail.com>
2014-04-17 00:20:19 -0400

    working SVD

--- java/SVD/src/org/rpiflix/svd/SVD.java
+++ java/SVD/src/org/rpiflix/svd/SVD.java
@@ -1,6 +1,5 @@
 package org.rpiflix.svd;
 
-
 public class SVD {
 	private final short[] rating_movie;
 	private final int[] rating_customer;
@@ -14,7 +13,12 @@ public class SVD {
 	private final int features;
 	
 	private final double[][] movieFeatures;
+	private final boolean[] changedMovie;
+//	private final short[] movieIDs;
+	
 	private final double[][] customerFeatures;
+	private final boolean[] changedCustomer;
+//	private final int[] customerIDs;
 	
 	public SVD(int features, int rating_count, int movie_count, int customer_count,
 			int maxCustomer,
@@ -32,18 +36,61 @@ public class SVD {
 		movieFeatures = new double[features][movie_count];
 		customerFeatures = new double[features][customer_count];
 		
+		changedMovie = new boolean[movie_count];
+		changedCustomer = new boolean[customer_count];
+		
 		customer_map = new int[maxCustomer + 1];
 		
 		System.out.println("learn rate: " + learnRate + ", regRate: " + regRate);
 	}
 	
-	public void setRatings(short[] rating_movie, int[] rating_customer, byte[] rating) {
-		for(int x = 0; x < rating_rating.length; x++) {
-			this.rating_movie[x] = rating_movie[x];
-			this.rating_customer[x] = rating_customer[x];
-			this.rating_rating[x] = rating[x];
-		}		
+	public void setRatings(int offset, short[] rating_movie, int[] rating_customer, byte[] rating) {
+		System.arraycopy(rating_movie, 0, this.rating_movie, offset, rating_movie.length);
+		System.arraycopy(rating_customer, 0, this.rating_customer, offset, rating_customer.length);
+		System.arraycopy(rating, 0, this.rating_rating, offset, rating.length);
+		
+//		for(int x = 0; x < rating_movie.length; x++) {
+//			this.rating_movie[offset + x] = rating_movie[x];
+//			this.rating_customer[offset + x] = rating_customer[x];
+//			this.rating_rating[offset + x] = rating[x];
+//		}		
 	}
+	
+	public int minRating() {
+		int min = 10;
+		
+		for(int x = 0; x < rating_rating.length; x++)
+			if(rating_rating[x] < min)
+				min = rating_rating[x];
+		
+		return min;
+	}
+	
+	public int maxRating() {
+		int max = -1;
+		
+		for(int x = 0; x < rating_rating.length; x++)
+			if(rating_rating[x] > max)
+				max = rating_rating[x];
+		
+		return max;
+	}
+	
+	
+//	public void setRatingMovies(short[] rating_movie) {
+//		for(int x = 0; x < rating_movie.length; x++)
+//			this.rating_movie[x] = rating_movie[x];
+//	}
+//	
+//	public void setRatingCustomers(int[] rating_customer) {
+//		for(int x = 0; x < rating_customer.length; x++)
+//			this.rating_customer[x] = rating_customer[x];
+//	}
+//	
+//	public void setRatingRatings(byte[] rating_rating) {
+//		for(int x = 0; x < rating_rating.length; x++)
+//			this.rating_rating[x] = rating_rating[x];
+//	}
 	
 	public void setCustomerMap(int[] customerMap) {
 		for(int x = 0; x < customer_map.length; x++)
@@ -56,10 +103,38 @@ public class SVD {
 				this.movieFeatures[f][m] = movieFeatures[m][f + 1];
 	}
 	
+	public double[][] getMovieFeatures() {
+		double[][] ret = new double[movieFeatures[0].length][features];
+		
+		for(int f = 0; f < features; f++)
+			for(int m = 0; m < movieFeatures[0].length; m++)
+				ret[m][f] = movieFeatures[f][m];
+		
+		return ret;
+	}
+	
+	public boolean[] getChangedMovies() {
+		return changedMovie;
+	}
+	
 	public void setCustomerFeatures(double[][] customerFeatures) {
 		for(int f = 0; f < features; f++)
 			for(int c = 0; c < customerFeatures.length; c++)
 				this.customerFeatures[f][c] = customerFeatures[c][f + 1];
+	}
+	
+	public double[][] getCustomerFeatures() {
+		double[][] ret = new double[customerFeatures[0].length][features];
+		
+		for(int f = 0; f < features; f++)
+			for(int c = 0; c < customerFeatures[0].length; c++)
+				ret[c][f] = customerFeatures[f][c];
+		
+		return ret;
+	}
+	
+	public boolean[] getChangedCustomers() {
+		return changedCustomer;
 	}
 	
 	public final double calcPred(final int movie, final int customer) {
@@ -109,8 +184,11 @@ public class SVD {
 				movieFeatures[f][mID] += learnRate * (err * ctemp - regRate * mtemp);
 				customerFeatures[f][cID] += learnRate * (err * mtemp - regRate * ctemp);
 			}
+			
+			changedMovie[mID] = true;
+			changedCustomer[cID] = true;
 		}
 		
-		return sse;
+		return Math.sqrt(sse / rating_rating.length);
 	}
 }
--- lib/CF/SVD.rb
+++ lib/CF/SVD.rb
@@ -1,5 +1,3 @@
-# require 'matrix'
-
 require 'java'
 require 'lib/java/SVD.jar'
 
@@ -23,116 +21,6 @@ module CF
 			self.table_name_prefix = 'svd_'
 		end
 	
-		def refine_customers ids
-			ActiveRecord::Base.connection_pool.with_connection do |conn|
-				# ids.each do |c|
-					sql = "
-						update svd_customer_features
-						set feat1 = svd_customer_features.feat1 + adj1
-						, feat2 = svd_customer_features.feat2 + adj2
-						, feat3 = svd_customer_features.feat3 + adj3
-						, feat4 = svd_customer_features.feat4 + adj4
-						, feat5 = svd_customer_features.feat5 + adj5
-
-						from 
-						(	select pred_cust
-							, avg(0.001 * err * feat1) as adj1
-							, avg(0.001 * err * feat2) as adj2
-							, avg(0.001 * err * feat3) as adj3
-							, avg(0.001 * err * feat4) as adj4
-							, avg(0.001 * err * feat5) as adj5
-							from
-							(	select rating - pred as err, mout.id as mid, pred_movie, pred_cust
-
-								FROM svd_movie_features mout
-							
-								left join 
-								lateral (select svd_movie_features.feat1 * svd_customer_features.feat1
-									+ svd_movie_features.feat2 * svd_customer_features.feat2
-									+ svd_movie_features.feat3 * svd_customer_features.feat3
-									+ svd_movie_features.feat4 * svd_customer_features.feat4
-									+ svd_movie_features.feat5 * svd_customer_features.feat5 as pred
-									, svd_movie_features.id as pred_movie
-									, svd_customer_features.id as pred_cust
-									, ratings.rating
-
-									from svd_movie_features 
-									cross join svd_customer_features
-									left join ratings on ratings.movie = svd_movie_features.id and ratings.customer = svd_customer_features.id
-
-									where svd_movie_features.id = mout.id
-									and svd_customer_features.id in (select distinct customer from ratings where movie = mout.id)
-								) p on mout.id = p.pred_movie
-								where mout.id BETWEEN 1 and 20
-							) p2
-							left join svd_customer_features on p2.pred_cust = svd_customer_features.id
-
-							group by p2.pred_cust
-						) p3
-
-						 where svd_customer_features.id = pred_cust
-						 "
-					
-					conn.execute sql
-				# end
-			end
-		end
-	
-		def refine_movies ids
-			ActiveRecord::Base.connection_pool.with_connection do |conn|
-				# ids.each do |m|
-					sql = "update svd_movie_features
-						set feat1 = svd_movie_features.feat1 + adj1
-						, feat2 = svd_movie_features.feat2 + adj2
-						, feat3 = svd_movie_features.feat3 + adj3
-						, feat4 = svd_movie_features.feat4 + adj4
-						, feat5 = svd_movie_features.feat5 + adj5
-
-						from 
-						(	select pred_movie
-							, avg(0.001 * err * feat1) as adj1
-							, avg(0.001 * err * feat2) as adj2
-							, avg(0.001 * err * feat3) as adj3
-							, avg(0.001 * err * feat4) as adj4
-							, avg(0.001 * err * feat5) as adj5
-							from
-							(	select rating - pred as err, mout.id as mid, pred_movie, pred_cust
-
-								FROM svd_movie_features mout
-							
-								left join 
-								lateral (select svd_movie_features.feat1 * svd_customer_features.feat1
-									+ svd_movie_features.feat2 * svd_customer_features.feat2
-									+ svd_movie_features.feat3 * svd_customer_features.feat3
-									+ svd_movie_features.feat4 * svd_customer_features.feat4
-									+ svd_movie_features.feat5 * svd_customer_features.feat5 as pred
-									, svd_movie_features.id as pred_movie
-									, svd_customer_features.id as pred_cust
-									, ratings.rating
-
-									from svd_movie_features 
-									cross join svd_customer_features
-									left join ratings on ratings.movie = svd_movie_features.id and ratings.customer = svd_customer_features.id
-
-									where svd_movie_features.id = mout.id
-									and svd_customer_features.id in (select distinct customer from ratings where movie = mout.id)
-								) p on mout.id = p.pred_movie
-								
-								where mout.id BETWEEN 1 and 20
-							) p2
-							left join svd_customer_features on p2.pred_cust = svd_customer_features.id
-
-							group by p2.pred_movie
-						) p3
-
-
-						 where svd_movie_features.id = pred_movie
-						"
-					conn.execute sql
-				# end
-			end
-		end
-	
 		def pred_single(mf, cf)
 			pred = 0.0
 			FEATURE_RANGE.each do |f|
@@ -142,200 +30,72 @@ module CF
 			pred
 		end
 	
-		def train_single(mf, cf, rating)
-			pred = 0.0
-			FEATURE_RANGE.each do |f|
-				pred += mf[f] * cf[f]
-			end
-			
-			err = rating - pred
-			
-			# puts err
-
-			FEATURE_RANGE.each do |f|
-				mtemp = mf[f]
-				ctemp = cf[f]
-				
-				# puts "mtemp: #{mtemp}, ctemp: #{ctemp}"
-				
-				# mf[f] += LRATE * (err * ctemp - KREG * mtemp)
-				# cf[f] += LRATE * (err * mtemp - KREG * ctemp)
-				
-				mf[f] += LRATE * (err * ctemp)
-				cf[f] += LRATE * (err * mtemp)
-			end
-			
-			err ** 2
-		end
-	
 		def train_do
-			ratings = Rating.where(:movie => 1..30).pluck(:movie, :customer, :rating)
-			# records = Rating.where(:movie => 1..3)
-			
-			puts 'records: ' + ratings.size.to_s
-		
-			# movie_ids = ratings.map{|r| r[0]}.uniq
-			# cust_ids = ratings.map{|r| r[1]}.uniq
-		
-			# puts cust_ids.to_s
-
-
-			# refine_movies movie_ids
-			# refine_customers cust_ids
-		
+			rating_count = Rating.count
+			puts 'records: ' + rating_count.to_s
 		
 			m_feat = MovieFeature.order(:id).pluck(:id, *FEATURE_COLUMNS)
 			c_feat = CustomerFeature.order(:id).pluck(:id, *FEATURE_COLUMNS)
 			
-			# cust id -> position lookup; to avoid custID searches
-			cust_map = Array.new(c_feat.last[0] + 1)
-			c_feat.each_index{|i| cust_map[c_feat[i][0]] = i}
+			jSVD = org.rpiflix.svd.SVD.new(FEATURES, rating_count, m_feat.size, c_feat.size, c_feat.last[0], LRATE, KREG)		
+						
+			puts 'min: ' + jSVD.minRating.to_s
+			puts 'max: ' + jSVD.maxRating.to_s
 			
+			offset = 0
+			m_feat.map{|f| f[0]}.each_slice(200) do |s|
+				# ratings = Rating.where(:movie => s).pluck(:movie, :customer, :rating)
+				
+				range = Range.new(s.min, s.max)
+				
+				r0 = Rating.order(:movie, :customer).where(:movie => range).pluck(:movie)
+				r1 = Rating.order(:movie, :customer).where(:movie => range).pluck(:customer)
+				r2 = Rating.order(:movie, :customer).where(:movie => range).pluck(:rating)
+				
+				# jSVD.setRatings(offset, ratings.map{|r| r[0]}, ratings.map{|r| r[1]}, ratings.map{|r| r[2]})
+				jSVD.setRatings(offset, r0, r1, r2)
+				offset += r0.size
+			end
 			
-			jSVD = org.rpiflix.svd.SVD.new(FEATURES, ratings.size, m_feat.size, c_feat.size, c_feat.last[0], LRATE, KREG)
-			
-			
-			r0 = ratings.map{|r| r[0]}
-			r1 = ratings.map{|r| r[1]}
-			r2 = ratings.map{|r| r[2]}
-			
-			jSVD.setRatings(r0, r1, r2)
-			
+			puts 'min: ' + jSVD.minRating.to_s
+			puts 'max: ' + jSVD.maxRating.to_s
+						
 			jSVD.setMovieFeatures(m_feat)
 			jSVD.setCustomerFeatures(c_feat)
 			
-			jSVD.setCustomerMap(cust_map)
+			jSVD.setCustomerMap(make_customer_map(c_feat))
 			
-			sseJ = jSVD.calcSSE
-			puts 'sseJ: ' + sseJ.to_s
-			
-			sseR = 0.0
-			ratings.each do |r|
-				mf = m_feat[r[0] - 1]
-				cf = c_feat[cust_map[r[1]]]
-				sseR += (r[2] - pred_single(mf, cf)) ** 2
+			100.times do 
+				t_rmse = jSVD.train
+				puts 'train rmse: ' + t_rmse.to_s
 			end
 			
-			puts 'sseR: ' + sseR.to_s
-			
-			# r = ratings.first
-			# mf = m_feat[r[0] - 1]
-			# if mf[0] != r[0]
-				# raise "unexpected movie id: #{mf[0]} vs. #{r[0]}"
-			# end
+			m_feat2 = jSVD.getMovieFeatures.to_a
+			m_changed = jSVD.getChangedMovies.to_a
 
-			# cf = c_feat[cust_map[r[1]]]
-			# if cf[0] != r[1]
-				# raise "unexpected customer id: #{cf[0]} vs. #{r[1]}"
-			# end
+			c_feat2 = jSVD.getCustomerFeatures.to_a
+			c_changed = jSVD.getChangedCustomers.to_a
 			
-			# predR = pred_single(mf, cf)
-			# puts 'predR: ' + predR.to_s
-			
-			# predJ = jSVD.calcPred(r[0], r[1])
-			# puts 'predJ: ' + predJ.to_s
-			
-			
-			10.times do 
-				tsse = jSVD.train
-				puts 'tsse: ' + tsse.to_s
-			end
-			
-			
-			
-			# m_mat = Matrix.build(m_feat.size, FEATURES) do |row, col|
-				# m_feat[row][col + 1]
-			# end
-			
-			# c_mat = Matrix.build(c_feat.size, FEATURES) do |row, col|
-				# c_feat[row][col + 1]
-			# end
-			
-			# sumprod = FEATURE_COLUMNS.map{|f| MovieFeature.table_name + '.' + f + ' * ' + CustomerFeature.table_name + '.' + f}.join(' + ')		
-			
-			# sql = "SELECT movie, customer, #{sumprod} AS pred FROM #{Rating.table_name}
-				# JOIN #{MovieFeature.table_name} on #{Rating.table_name}.movie = #{MovieFeature.table_name}.id
-				# JOIN #{CustomerFeature.table_name} on #{Rating.table_name}.customer = #{CustomerFeature.table_name}.id
-				# WHERE movie < 20"
-				
-				# -- where movie=1
-				# ( SELECT movie, rating - #{global_mean} - #{CustomerBias.table_name}.bias AS bias FROM ratings
-					# JOIN #{CustomerBias.table_name} ON ratings.customer = #{CustomerBias.table_name}.id
-					# WHERE movie BETWEEN #{min} AND #{max}
-					# ) as t1
-				# group by movie"
-				
-				
-				
-				# select *, rating - pred as err
-				# from (select svd_movie_features.feat1 * svd_customer_features.feat1
-				# + svd_movie_features.feat2 * svd_customer_features.feat2
-				# + svd_movie_features.feat3 * svd_customer_features.feat3
-				# + svd_movie_features.feat4 * svd_customer_features.feat4
-				# + svd_movie_features.feat5 * svd_customer_features.feat5 as pred
-				# , svd_movie_features.id as pred_movie
-				# , svd_customer_features.id as pred_cust
-				# , ratings.rating
-
-				# from svd_movie_features 
-				# cross join svd_customer_features
-				# left join ratings on ratings.movie = svd_movie_features.id and ratings.customer = svd_customer_features.id
-				# ) p
-
-				# where p.pred_movie = 1
-				# and p.pred_cust in (select customer from ratings where movie=1)
-			
-			# puts sql
-			
-			
-			# records = Rating.order(:movie).first(10000)
-			# records = Rating.order(:movie).first(20)
-			# ratings = Rating.where(:movie => 1..199).pluck(:movie, :customer, :rating)
-			
-		
-			# raise 'err'
-		
-			(1..10).each do
-				sse = 0.0
-				
-				ratings.each do |r|				
-					# movie ids are consecutive
-					mf = m_feat[r[0] - 1]
-					if mf[0] != r[0]
-						raise "unexpected movie id: #{mf[0]} vs. #{r[0]}"
-					end
-					
-					# customer ids not consecutive, need to search
-					# cf = c_feat.bsearch{|e| r[1] <=> e[0]}
-					cf = c_feat[cust_map[r[1]]]
-					if cf[0] != r[1]
-						raise "unexpected customer id: #{cf[0]} vs. #{r[1]}"
-					end
-				
-					sse += train_single(mf, cf, r[2])
-					
-					# mf[FEATURES + 1] = true
-					# cf[FEATURES + 1] = true
+			m_update = []
+			m_feat.each_index do |i|
+				if m_changed[i]
+					m_update << [m_feat[i][0], *m_feat2[i]]
 				end
-				
-				puts "training SSE: #{sse}"
+			end
+			
+			c_update = []
+			c_feat.each_index do |i|
+				if c_changed[i]
+					c_update << [c_feat[i][0], *c_feat2[i]]
+				end
 			end
 			
 			
-			raise 'errr2'
+			MovieFeature.delete(m_update.map{|f| f[0]})
+			MovieFeature.import([:id] + FEATURE_COLUMNS, m_update, :validate => false)
 			
-			m_feat = m_feat.select{|m| m[FEATURES + 1]}.map{|f| f.slice 0..FEATURES}
-			c_feat = c_feat.select{|c| c[FEATURES + 1]}.map{|f| f.slice 0..FEATURES}
-			
-			puts m_feat.to_s
-			
-			raise 'err'
-			
-			MovieFeature.delete(m_feat.map{|f| f[0]})
-			MovieFeature.import([:id] + FEATURE_COLUMNS, m_feat, :validate => false)
-			
-			CustomerFeature.delete(c_feat.map{|f| f[0]})
-			CustomerFeature.import([:id] + FEATURE_COLUMNS, c_feat, :validate => false)
+			CustomerFeature.delete(c_update.map{|f| f[0]})
+			CustomerFeature.import([:id] + FEATURE_COLUMNS, c_update, :validate => false)
 		end
 		
 		def reset_do
@@ -364,79 +124,34 @@ module CF
 			values = customers.map{|c| [c] + starting}
 			CustomerFeature.import( [:id] + FEATURE_COLUMNS, values, :validate => false)
 		end
-	
-		# def pred(movie, customer)
-			# mf = MovieFeatures.find(movie)
-			# cf = CustomerFeatures.find(customer)
-			
-			# sum = 0.0
-			# FEATURE_COLUMNS.each do |f|
-				# sum += mf[c] * cf[c]
-			# end
-			
-			# return sum
-		# end
+
 	
 		def rate(movie, customer, date)
+			populate_caches
 			
-			# pred = @cache_global_mean + @cache_customer[customer] + @cache_movie[movie]
+			mf = @cache_movies[movie - 1]
+			cf = @cache_customers[@cache_customer_map[customer]]
+
+			pred = pred_single(mf, cf)
 			
-			# [[pred, 5].min, 1].max
+			[[pred, 5].min, 1].max
 		end
 	
+	private
+		def make_customer_map cust_features
+			# cust id -> position lookup; to avoid custID searches
+			cust_map = Array.new(cust_features.last[0] + 1)
+			cust_features.each_index{|i| cust_map[cust_features[i][0]] = i}
+			
+			cust_map
+		end
+	
+		def populate_caches
+			if @cache_movies.nil?
+				@cache_movies = MovieFeature.order(:id).pluck(:id, *FEATURE_COLUMNS)
+				@cache_customers = CustomerFeature.order(:id).pluck(:id, *FEATURE_COLUMNS)
+				@cache_customer_map = make_customer_map(@cache_customers)
+			end
+		end
 	end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# update svd_movie_features
-# set feat1 = svd_movie_features.feat1 + adj1
-# , feat2 = svd_movie_features.feat1 + adj2
-# , feat3 = svd_movie_features.feat1 + adj3
-# , feat4 = svd_movie_features.feat1 + adj4
-# , feat5 = svd_movie_features.feat1 + adj5
-
-# from 
-# (	select *, err
-	# , 0.001 * err * feat1 as adj1
-	# , 0.001 * err * feat2 as adj2
-	# , 0.001 * err * feat3 as adj3
-	# , 0.001 * err * feat4 as adj4
-	# , 0.001 * err * feat5 as adj5
-	# from
-	# (	select rating - pred as err, pred_movie, pred_cust
-		# from (select svd_movie_features.feat1 * svd_customer_features.feat1
-		# + svd_movie_features.feat2 * svd_customer_features.feat2
-		# + svd_movie_features.feat3 * svd_customer_features.feat3
-		# + svd_movie_features.feat4 * svd_customer_features.feat4
-		# + svd_movie_features.feat5 * svd_customer_features.feat5 as pred
-		# , svd_movie_features.id as pred_movie
-		# , svd_customer_features.id as pred_cust
-		# , ratings.rating
-
-		# from svd_movie_features 
-		# cross join svd_customer_features
-		# left join ratings on ratings.movie = svd_movie_features.id and ratings.customer = svd_customer_features.id
-		# ) p
-	# ) p2
-	# left join svd_customer_features on p2.pred_cust = svd_customer_features.id
-
-	# where p2.pred_movie = 1
-	# and p2.pred_cust in (915, 2442, 3321)
-# ) p3
-# where svd_movie_features.id = 1
-
-
-
-# -- and p2.pred_cust in (select customer from ratings where movie=1)
