5c790a100dcbad514c0c655ac6f2ed3dd19e1f09
Justin Zipkin <justin.zipkin@gmail.com>
2014-04-21 15:20:03 -0400

    where on key movie

--- app/models/Pearson.rb
+++ app/models/Pearson.rb
@@ -2,11 +2,11 @@ class Pearson < ActiveRecord::Base
   self.table_name = "pearson"
   
   def self.get_similar_movies(movieid, number_similar)
-    similar = Pearson.select("movie1")
-            .where("movie2=#{movieid}")
+    similar = Pearson.select("movie2")
+            .where("movie1=#{movieid}")
             .order("pearson desc")
             .limit(number_similar)
     
-    Movie.find(similar.collect{ |m| m.movie1 })
+    Movie.find(similar.collect{ |m| m.movie2 })
   end
 end
